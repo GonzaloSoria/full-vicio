027811a6b94d7fb504777e653aad8253fb86f529
GonzaloSoria <gonzalo.sooria@gmail.com>
2022-05-30 17:18:46 -0300

    changed name component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from './components/Navbar'
 import Home from './pages/Home/Home';
-import ItemListcontainer from './pages/ItemListContainer';
+import ItemListcontainer from './pages/ItemListContainer/ItemListContainer';
 
 const App = () => {
   return (
