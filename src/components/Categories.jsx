216f88c5cc2fc0c1d2acd0e3072903235e739a9e
GonzaloSoria <gonzalo.sooria@gmail.com>
2022-05-31 16:12:33 -0300

    styles fixed to categories component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,22 +3,25 @@ const Categories = () => {
         <>
             <div className="container-fluid">
                 <div className="container">
-                    <div className="row">
-                        <h2 className="categorias mb-5">Categorias</h2>
-                        <a href="#" className="col-12 col-md-4 bg-rpg py-5 mb-3 mb-md-0">
+                    <h2 className="categorias mb-5">Categorias</h2>
+                    <div className="row d-flex justify-content-center">
+                        <a href="#" className="col-5 col-md-4 bg-rpg py-5 mb-3 mb-md-0">
                             <h2 className="py-md-5">RPG</h2>
                         </a>
-                        <a href="#" className="col-12 col-md-8 bg-strategy py-5 mb-3 mb-md-0">
+                        <a href="#" className="col-5 offset-1 offset-md-0 col-md-8 bg-strategy py-5 mb-3 mb-md-0">
                             <h2 className="py-md-5">STRATEGY</h2>
                         </a>
-                        <a href="#" className="col-12 col-md-4 bg-shooter py-5 mb-3 mb-md-0">
+                        <a href="#" className="col-5 col-md-3 bg-shooter py-5 mb-3 mb-md-0">
                             <h2 className="py-md-5">SHOOTER</h2>
                         </a>
-                        <a href="#" className="col-12 col-md-4 bg-simulator py-5 mb-3 mb-md-0">
+                        <a href="#" className="col-5 offset-1 offset-md-0 col-md-3 bg-simulator py-5 mb-3 mb-md-0">
                             <h2 className="py-md-5">SIMULATOR</h2>
                         </a>
-                        <a href="#" className="col-12 col-md-4 bg-sports py-5 mb-3 mb-md-0">
+                        <a href="#" className="col-5 col-md-3 bg-sports py-5 mb-3 mb-md-0">
                             <h2 className="py-md-5">SPORTS</h2>
+                        </a>
+                        <a href="#" className="col-5 offset-1 offset-md-0 col-md-3 bg-royal py-5 mb-3 mb-md-0">
+                            <h2 className="py-md-5">ROYAL</h2>
                         </a>
                     </div>
                 </div>
