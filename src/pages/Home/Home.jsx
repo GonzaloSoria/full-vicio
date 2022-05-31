3285b9e4f72e8874e7f86868f918692964eca149
GonzaloSoria <gonzalo.sooria@gmail.com>
2022-05-31 13:13:33 -0300

    Added reset styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,9 +3,7 @@ import Slide from "../../components/SlideHome";
 const Home = () => {
     return (
         <>
-         <div style={{backgroundColor: '#212429'}}>
             <Slide />
-         </div>
         </>
     )
 }
