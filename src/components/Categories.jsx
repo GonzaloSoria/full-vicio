642f509b46d9e2d767666c3b5567e01baebebdfe
GonzaloSoria <gonzalo.sooria@gmail.com>
2022-05-31 18:12:46 -0300

    changed row categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,26 +1,26 @@
 const Categories = () => {
     return (
         <>
-            <div className="container-fluid">
+            <div className="container-fluid px-0">
                 <div className="container">
                     <h2 className="categorias mb-5">Categorias</h2>
-                    <div className="row d-flex justify-content-center">
-                        <a href="#" className="col-5 col-md-4 bg-rpg py-5 mb-3 mb-md-0">
+                    <div className="row gx-3 d-flex justify-content-center">
+                        <a href="#" className="col-5 col-md-2 bg-rpg py-5 mb-3 mb-md-0">
                             <h2 className="py-md-5">RPG</h2>
                         </a>
-                        <a href="#" className="col-5 offset-1 offset-md-0 col-md-8 bg-strategy py-5 mb-3 mb-md-0">
+                        <a href="#" className="col-5 offset-1 offset-md-0 col-md-2 bg-strategy py-5 mb-3 mb-md-0">
                             <h2 className="py-md-5">STRATEGY</h2>
                         </a>
-                        <a href="#" className="col-5 col-md-3 bg-shooter py-5 mb-3 mb-md-0">
+                        <a href="#" className="col-5 col-md-2 bg-shooter py-5 mb-3 mb-md-0">
                             <h2 className="py-md-5">SHOOTER</h2>
                         </a>
-                        <a href="#" className="col-5 offset-1 offset-md-0 col-md-3 bg-simulator py-5 mb-3 mb-md-0">
+                        <a href="#" className="col-5 offset-1 offset-md-0 col-md-2 bg-simulator py-5 mb-3 mb-md-0">
                             <h2 className="py-md-5">SIMULATOR</h2>
                         </a>
-                        <a href="#" className="col-5 col-md-3 bg-sports py-5 mb-3 mb-md-0">
+                        <a href="#" className="col-5 col-md-2 bg-sports py-5 mb-3 mb-md-0">
                             <h2 className="py-md-5">SPORTS</h2>
                         </a>
-                        <a href="#" className="col-5 offset-1 offset-md-0 col-md-3 bg-royal py-5 mb-3 mb-md-0">
+                        <a href="#" className="col-5 offset-1 offset-md-0 col-md-2 bg-royal py-5 mb-3 mb-md-0">
                             <h2 className="py-md-5">ROYAL</h2>
                         </a>
                     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,51 +4,51 @@ import FavWidget from "./FavWidget";
 const Navbar = () => {
     return (
         <>
-        <header className="container-fluid py-4 primary-bg w-100">
-            <div className="container">
-                <nav className="d-flex justify-content-between">
-                    <div className="d-flex align-items-center">
-                        <a  data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample" className="">
-                            <label className="lbl-menu">
-                                <span className="span1"></span>
-                                <span className="span2"></span>
-                                <span className="span3"></span>
-                            </label>
-                        </a>
-                        <div className="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
-                            <div className="offcanvas-header">
-                                <h5 className="offcanvas-title menu" id="offcanvasExampleLabel">Menu</h5>
-                                <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            <header className="container-fluid py-4 primary-bg w-100">
+                <div className="container">
+                    <nav className="d-flex justify-content-between">
+                        <div className="d-flex align-items-center">
+                            <a  data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample" className="">
+                                <label className="lbl-menu">
+                                    <span className="span1"></span>
+                                    <span className="span2"></span>
+                                    <span className="span3"></span>
+                                </label>
+                            </a>
+                            <div className="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                                <div className="offcanvas-header">
+                                    <h5 className="offcanvas-title menu" id="offcanvasExampleLabel">Menu</h5>
+                                    <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                </div>
+                                <div className="offcanvas-body">
+                                    <nav className="nav">
+                                        <ul>
+                                            <li><a href="#">Home</a></li>
+                                            <li><a href="#">Carrito</a></li>
+                                            <li><a href="#">Favoritos</a></li>
+                                            <li><a href="#">Consolas</a></li>
+                                            <li><a href="#">Categorias</a></li>
+                                        </ul>
+                                    </nav>
+                                    <a href="#"><h1 className="brand d-block d-md-none border-none">Fullvicio.</h1></a>
+                                </div>
                             </div>
-                            <div className="offcanvas-body">
-                                <nav className="nav">
-                                    <ul>
-                                        <li><a href="#">Home</a></li>
-                                        <li><a href="#">Carrito</a></li>
-                                        <li><a href="#">Favoritos</a></li>
-                                        <li><a href="#">Consolas</a></li>
-                                        <li><a href="#">Categorias</a></li>
-                                    </ul>
-                                </nav>
-                                <a href="#"><h1 className="brand d-block d-md-none border-none">Fullvicio.</h1></a>
+                            <a href="#"><h1 className="brand d-none d-md-block">Fullvicio.</h1></a>
+                        </div>
+                        <div className="d-flex justifi-content-center">
+                            <form action="" className="d-flex justify-content-center align-items-center search-form">
+                                <input type="search" placeholder="Buscar"/>
+                                <input type="image" src="assets/lupa.png" className="lupa"/>
+                            </form>
+                            <div className="bg-juegos mx-2">
+                                <a href="#" className="juegos">Juegos</a>
                             </div>
+                            <FavWidget />
+                            <CartWidget />
                         </div>
-                        <a href="#"><h1 className="brand d-none d-md-block">Fullvicio.</h1></a>
-                    </div>
-                    <div className="d-flex justifi-content-center">
-                        <form action="" className="d-flex justify-content-center align-items-center search-form">
-                            <input type="search" placeholder="Buscar"/>
-                            <input type="image" src="assets/lupa.png" className="lupa"/>
-                        </form>
-                        <div className="bg-juegos mx-2">
-                            <a href="#" className="juegos">Juegos</a>
-                        </div>
-                        <FavWidget />
-                        <CartWidget />
-                    </div>
-                </nav>
-            </div>
-        </header>
+                    </nav>
+                </div>
+            </header>
         </>
     )
 }
