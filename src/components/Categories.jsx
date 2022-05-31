b4f6e8dce343873dd2d980b4cb30ac19d0abfc0a
GonzaloSoria <gonzalo.sooria@gmail.com>
2022-05-31 15:29:16 -0300

    Created Categories components

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,28 @@
 const Categories = () => {
     return (
         <>
-        <div className="container-fluid">
-            <div className="container">
-                <h2 className="categorias mb-4">Categorias</h2>
-                <div className="row">
-                    <a href="#" className="col-12 col-md-4 bg-rpg py-5">
-                        <h2 className="py-md-5">RPG</h2>
-                    </a>
+            <div className="container-fluid">
+                <div className="container">
+                    <div className="row">
+                        <h2 className="categorias mb-5">Categorias</h2>
+                        <a href="#" className="col-12 col-md-4 bg-rpg py-5 mb-3 mb-md-0">
+                            <h2 className="py-md-5">RPG</h2>
+                        </a>
+                        <a href="#" className="col-12 col-md-8 bg-strategy py-5 mb-3 mb-md-0">
+                            <h2 className="py-md-5">STRATEGY</h2>
+                        </a>
+                        <a href="#" className="col-12 col-md-4 bg-shooter py-5 mb-3 mb-md-0">
+                            <h2 className="py-md-5">SHOOTER</h2>
+                        </a>
+                        <a href="#" className="col-12 col-md-4 bg-simulator py-5 mb-3 mb-md-0">
+                            <h2 className="py-md-5">SIMULATOR</h2>
+                        </a>
+                        <a href="#" className="col-12 col-md-4 bg-sports py-5 mb-3 mb-md-0">
+                            <h2 className="py-md-5">SPORTS</h2>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
