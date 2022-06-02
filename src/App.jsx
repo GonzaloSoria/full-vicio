9da2875fe45f99abdc9f0eaa71a7e48350854f60
gonzalo soria <gonzalo.soria@mediamonks.com>
2022-06-02 18:16:07 -0300

    Created button count

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import ItemCount from './components/ItemCount';
 import Navbar from './components/Navbar'
 import Home from './pages/Home/Home';
 import ItemListcontainer from './pages/ItemListContainer/ItemListContainer';
@@ -8,6 +9,7 @@ const App = () => {
       <Navbar />
       <Home />
       <ItemListcontainer greeting={'Sitio web en construccion'}/>
+      <ItemCount stock={5}/>
     </>
   );
 }
--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,7 +1,21 @@
-const ItemCount = () => {
+import { useEffect, useState } from "react";
+
+const ItemCount = ({stock}) => {
+    const [count, setCount] = useState(1)
+
+    const add = () => {
+        count == stock ? alert('No hay mas stock') : setCount(count + 1);
+    };
+
+    const rest = () => {
+        count == 1 ? alert('Cantidad vacia') : setCount(count - 1);
+    };
+
     return (
         <>
-        
+         <button onClick={rest}>-</button>
+         <p style={{color: '#fff'}}>{count}</p>
+         <button onClick={add}>+</button>
         </>
     )
 }
