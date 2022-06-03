a3f554f702c8ed039efabc01c9e611c0ed35c0e0
GonzaloSoria <gonzalo.sooria@gmail.com>
2022-06-03 19:09:46 -0300

    Updated Item component

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -23,9 +23,10 @@ const Item = () => {
             {
                 products.map((product) => {
                     return (
-                        <a key={product.id} href="#" className="col-6 col-md-2 item">
+                        <a key={product.id} href="#" className="col-6 col-md-2 my-3 item">
                             <img src={product.image} alt="" className="img-fluid"/>
                             <h2 className="mt-2">{product.title}</h2>
+                            <p>ARS ${product.price}</p>
                         </a>
                     )
                 })
--- src/database/database.js
+++ src/database/database.js
@@ -1,43 +1,43 @@
 export const data = [{
-    title: 'The Elder Scroll V Skyrim',
+    title: 'The Elder Scroll V: Skyrim',
     description: 'he Elder Scrolls V: Skyrim is an action role-playing video game developed by Bethesda Game Studios and published by Bethesda Softworks.',
     price: 500,
     image: 'assets/games/skyrim.jpg',
     popular: true,
     id: 1
 },{
-    title: 'The Witcher 3',
+    title: 'FARCRY 6',
     description: 'The Witcher 3: Wild Hunt is an action role-playing video game developed by Polish developer CD Projekt Red, and first published in 2015.',
     price: 800,
-    image: 'assets/games/skyrim.jpg',
+    image: 'assets/games/farcry6.jpg',
     popular: true,
     id: 2
 },{
-    title: 'The Witcher 3',
+    title: 'Star Wars: Battlefront 2',
     description: 'The Witcher 3: Wild Hunt is an action role-playing video game developed by Polish developer CD Projekt Red, and first published in 2015.',
     price: 800,
-    image: 'assets/games/skyrim.jpg',
+    image: 'assets/games/battlefront2.jpg',
     popular: true,
     id: 3
 },{
-    title: 'The Witcher 3',
+    title: 'God of War: Ragnarok',
     description: 'The Witcher 3: Wild Hunt is an action role-playing video game developed by Polish developer CD Projekt Red, and first published in 2015.',
     price: 800,
-    image: 'assets/games/skyrim.jpg',
+    image: 'assets/games/godofwar.jpg',
     popular: true,
     id: 4
 },{
-    title: 'The Witcher 3',
+    title: 'Dark Souls 3',
     description: 'The Witcher 3: Wild Hunt is an action role-playing video game developed by Polish developer CD Projekt Red, and first published in 2015.',
     price: 800,
-    image: 'assets/games/skyrim.jpg',
+    image: 'assets/games/darksouls3.jpg',
     popular: true,
     id: 5
 },{
-    title: 'The Witcher 3',
+    title: 'Grand Theft Auto V',
     description: 'The Witcher 3: Wild Hunt is an action role-playing video game developed by Polish developer CD Projekt Red, and first published in 2015.',
     price: 800,
-    image: 'assets/games/skyrim.jpg',
+    image: 'assets/games/gta5.jpg',
     popular: true,
     id: 6
 }];
