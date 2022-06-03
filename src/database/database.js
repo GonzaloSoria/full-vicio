62b797326214fe5ac1a45eeab43949b4c4e32ca4
gonzalo soria <gonzalo.soria@mediamonks.com>
2022-06-03 16:57:55 -0300

    components created

---
+++ src/database/database.js
@@ -0,0 +1,15 @@
+export const data = [{
+    title: 'The Elder Scroll V Skyrim',
+    description: 'he Elder Scrolls V: Skyrim is an action role-playing video game developed by Bethesda Game Studios and published by Bethesda Softworks.',
+    price: 500,
+    image: '',
+    popular: true,
+    id: 1
+}, {
+    title: 'The Witcher 3',
+    description: 'The Witcher 3: Wild Hunt is an action role-playing video game developed by Polish developer CD Projekt Red, and first published in 2015.',
+    price: 800,
+    image: '',
+    popular: true,
+    id: 1
+}];
