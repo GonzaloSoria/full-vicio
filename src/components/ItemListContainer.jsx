e617f118a3fff914d872d90d4d6aa40ce7e9f16e
gonzalo soria <gonzalo.soria@mediamonks.com>
2022-06-06 11:50:15 -0300

    add validate on stock

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -18,7 +18,7 @@ const ItemListcontainer = () => {
     };
 
     const handleCart = () => {
-        setCart(cart + count)
+        cart == stock ? alert('No hay mas stock para agregar') : setCart(cart + count)
     };
 
     useEffect(() => {
