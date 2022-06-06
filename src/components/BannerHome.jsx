19c0e1f4c09dcf298cd605035f5e5842ad154a83
gonzalo soria <gonzalo.soria@mediamonks.com>
2022-06-06 17:54:54 -0300

    updated banner

--- src/components/BannerHome.jsx
+++ src/components/BannerHome.jsx
@@ -5,7 +5,7 @@ const BannerHome = () => {
             <div className="container my-2 container-banner">
                 <div className="row">
                     <div className="col-12 col-md-6 d-flex flex-column justify-content-center bg-gris">
-                        <h2 className="text-center pt-4">Descubri los mejores Juegos</h2>
+                        <h2 className="text-center pt-5 pt-md-3">Descubri los mejores Juegos</h2>
                         <p className="text-center mb-4">Encontrá los juegos que mas te gusten en las diferentes categorias</p>
                     </div>
                     <div className="col-12 col-md-6 bg-gris px-0 text-end">
