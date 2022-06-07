2ecb72979021430bcc8ba6390ce192e783ee255e
gonzalo soria <gonzalo.soria@mediamonks.com>
2022-06-07 10:05:52 -0300

    Links updated on SlideHome

--- src/components/SlideHome.jsx
+++ src/components/SlideHome.jsx
@@ -27,16 +27,16 @@ const SlideHome = () => {
                         <h2>Diablo IV</h2>
                         <p>Diablo IV es un próximo juego de rol de acción de exploración de mazmorras desarrollado y publicado por Blizzard Entertainment, el cuarto título de la serie Diablo.</p>
                         <div className="d-flex">
-                            <a href="#" className="comprar-ahora">COMPRAR AHORA</a>
+                            <a href="https://diablo4.blizzard.com/en-us/" target="_blank" className="comprar-ahora">MÁS INFO</a>
                         </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="bg-slide-game2">
                     <div className="d-flex flex-column justify-content-end px-3 h-100 pb-5">
-                        <h2>League of Leyends</h2>
+                        <h2>League of Legends</h2>
                         <p>League of Legends, comúnmente conocido como LOL, es un videojuego de arena de batalla multijugador en línea de 2009 desarrollado y publicado por Riot Games.</p>
                         <div className="d-flex">
-                            <a href="#" className="comprar-ahora">COMPRAR AHORA</a>
+                            <a href="https://signup.leagueoflegends.com/es-mx/signup/index#/" target="_blank" className="comprar-ahora">JUEGA GRATIS</a>
                         </div>
                     </div>
                 </SwiperSlide>
@@ -45,7 +45,7 @@ const SlideHome = () => {
                         <h2>Assasins Creed: Valhalla</h2>
                         <p>Assassin's Creed Valhalla es un videojuego de rol de acción de 2020 desarrollado por Ubisoft Montreal y publicado por Ubisoft.</p>
                         <div className="d-flex">
-                            <a href="#" className="comprar-ahora">COMPRAR AHORA</a>
+                            <a href="https://www.ubisoft.com/en-gb/game/assassins-creed/valhalla" target="_blank" className="comprar-ahora">MÁS INFO</a>
                         </div>
                     </div>
                 </SwiperSlide>
@@ -54,7 +54,7 @@ const SlideHome = () => {
                         <h2>Elden Ring</h2>
                         <p>Elden Ring es un juego de rol de acción desarrollado por FromSoftware y publicado por Bandai Namco Entertainment.</p>
                         <div className="d-flex">
-                            <a href="#" className="comprar-ahora">COMPRAR AHORA</a>
+                            <a href="https://en.bandainamcoent.eu/elden-ring/elden-ring" target="_blank" className="comprar-ahora">MÁS INFO</a>
                         </div>
                     </div>
                 </SwiperSlide>
