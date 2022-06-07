bc997409df19e8e8df810afc9dd8e62073e70f5d
gonzalo soria <gonzalo.soria@mediamonks.com>
2022-06-07 14:04:09 -0300

    updated sound

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,10 +1,10 @@
 const Item = ({title, image, price}) => {
-    const choose_game = new Audio();
-    choose_game.src = 'assets/sounds/choose_game.ogg';
+    const choose_item = new Audio();
+    choose_item.src = 'assets/sounds/choose_item.ogg';
 
     return (
         <>
-            <a onClick={() => choose_game.play()} href="#" className="col-6 col-md-2 my-3 item">
+            <a onClick={() => choose_item.play()} href="#" className="col-6 col-md-2 my-3 item">
                 <img src={image} alt="" className="img-fluid"/>
                 <h2 className="mt-2">{title}</h2>
                 <p>ARS ${price}</p>
