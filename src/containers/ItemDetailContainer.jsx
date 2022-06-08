c6b98c149b80a18d779ec2350137a0d58a21880b
GonzaloSoria <gonzalo.sooria@gmail.com>
2022-06-08 20:55:34 -0300

    Need to fix find method

--- src/containers/ItemDetailContainer.jsx
+++ src/containers/ItemDetailContainer.jsx
@@ -2,16 +2,16 @@ import { useState, useEffect } from "react";
 import { get_data } from "../helpers/get_data";
 
 const ItemDetailContainer = () => {
-    const [product, setProduct] = useState({})
-    const product_detail = product.find(product => product.id === "1");
+    const [product, setProduct] = useState([])
+    const product_detail = product.find(productDetail => productDetail.id == "1");
+    console.log(product_detail);
 
     useEffect(() => {
-      get_data(setProduct);
+      get_data(setProduct)
     }, [])
     
     return (
         <>
-            <p>{product_detail.title}</p>
         </>
     )
 }
