c4af7fdc5afc73af9aae1d4258afb3c56bb1e530
gonzalo soria <gonzalo.soria@mediamonks.com>
2022-06-08 16:33:20 -0300

    updated

--- src/components/BannerHome.jsx
+++ src/components/BannerHome.jsx
@@ -1,21 +1,19 @@
 const BannerHome = () => {
     return (
         <>
-        <div className="container-fluid pt-3 pt-md-1">
-            <div className="container my-2 container-banner">
-                <div className="row">
-                    <div className="col-12 col-md-6 d-flex flex-column justify-content-center bg-gris">
-                        <h2 className="text-center pt-5 pt-md-3">Descubri los mejores Juegos</h2>
-                        <p className="text-center mb-5 mb-md-4">Encontrá los juegos que mas te gusten en las diferentes categorias</p>
-                    </div>
-                    <div className="col-12 col-md-6 bg-gris px-0 text-center">
-                        <img src="assets/banner-caracters.png" alt="" className="img-fluid d-none d-md-block"/>
+            <div className="container-fluid pt-3 pt-md-1">
+                <div className="container my-2 container-banner">
+                    <div className="row">
+                        <div className="col-12 col-md-6 d-flex flex-column justify-content-center bg-gris">
+                            <h2 className="text-center pt-5 pt-md-3">Descubri los mejores Juegos</h2>
+                            <p className="text-center mb-5 mb-md-4">Encontrá los juegos que mas te gusten en las diferentes categorias</p>
+                        </div>
+                        <div className="col-12 col-md-6 bg-gris px-0 text-center">
+                            <img src="assets/banner-caracters.png" alt="" className="img-fluid d-none d-md-block"/>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        
-            
+            </div>           
         </>
     );
 };
