7e03063ae0081404e73f26b0a9a4df8df0615f24
gonzalo soria <gonzalo.soria@mediamonks.com>
2022-06-10 19:35:07 -0300

    Updated component

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -4,15 +4,15 @@ const ItemDetail = ({title, description, price, image, trailer, stock}) => {
             <div className="col-12 col-md-6 text-center mt-110">
                 <img src={image} alt={title} className="img-fluid"/> 
             </div>
-            <div className="col-12 col-md-6 game-detail-container mt-110">
+            <div className="col-12 col-md-6 mt-5 game-detail-container">
                 <h2>{title}</h2>
                 <p>{description}</p>
                 <p>Stock: {stock}</p>
                 <p className="price">${price} ARS</p>
             </div>
             <div className="col-12 col-md-6">
-                <div class="ratio ratio-16x9">
-                    <iframe src={trailer} title="YouTube video" allowfullscreen></iframe>
+                <div className="ratio ratio-16x9">
+                    <iframe src={trailer} title="YouTube video" allowFullScreen></iframe>
                 </div>
             </div>
         </>
