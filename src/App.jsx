22bc9d47bf7386fdc61538c273da34af95fff4ef
gonzalo soria <gonzalo.soria@mediamonks.com>
2022-06-14 12:59:20 -0300

    changed pages to routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Products from './pages/Products';
-import Details from './pages/Details';
+import Home from './routes/Home';
+import Products from './routes/Products';
+import Details from './routes/Details';
 
 
 const App = () => {
