d1b5083f43568d45863f216f3468dee4f30125ea
GonzaloSoria <gonzalo.sooria@gmail.com>
2022-06-14 20:41:50 -0300

    fixing

--- src/helpers/get_data_details.js
+++ src/helpers/get_data_details.js
@@ -1,7 +1,7 @@
 export const get_data_details = async (setProduct, id) => {
     const data = await fetch('../../database/DATABASE.json');
+    const data_parsed = await data.json();
     try {
-      const data_parsed = await data.json();
       const data_found = data_parsed.find(game => game.id === id);
       setProduct(data_found);
     } catch (error) {
--- src/helpers/get_popular_data.js
+++ src/helpers/get_popular_data.js
@@ -1,7 +1,7 @@
 export const get_popular_data = async (setPopularGames) => {
     const data = await fetch('../../database/DATABASE.json');
+    const data_parsed = await data.json();
     try {
-      const data_parsed = await data.json();
       const popular_game = data_parsed.filter(game => game.popular === true);
       setPopularGames(popular_game);
     } catch (error) {
