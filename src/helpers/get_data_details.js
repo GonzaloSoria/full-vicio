f9398edf5ffd380a173fe17cd769baf369f39e5d
GonzaloSoria <gonzalo.sooria@gmail.com>
2022-08-12 18:06:26 -0300

    added firebase

--- src/helpers/get_data_details.js
+++ src/helpers/get_data_details.js
@@ -1,10 +1,18 @@
-export const get_data_details = async (setProduct, id) => {
-    const data = await fetch('../../database/DATABASE.json');
-    const data_parsed = await data.json();
-    try {
-      const data_found = data_parsed.find(game => game.id === id);
-      setProduct(data_found);
-    } catch (error) {
-      console.log(error);
-    }
-}
+// export const get_data_details = async (setProduct, id) => {
+//     const data = await fetch('../../database/DATABASE.json');
+//     const data_parsed = await data.json();
+//     try {
+//       const data_found = data_parsed.find(game => game.id === id);
+//       setProduct(data_found);
+//     } catch (error) {
+//       console.log(error);
+//     }
+// }
+
+import { doc, getFirestore } from "firebase/firestore";
+
+
+export const get_data_details = async () => {
+  const db = getFirestore();
+  const querie_item = doc(db, '')
+};
