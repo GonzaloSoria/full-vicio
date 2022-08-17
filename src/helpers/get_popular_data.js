b1103dc91bee1f8656d8aab5f6a85401334fce6d
GonzaloSoria <gonzalo.sooria@gmail.com>
2022-08-17 20:48:45 -0300

    adding firabase on get popular data

--- src/helpers/get_popular_data.js
+++ src/helpers/get_popular_data.js
@@ -1,10 +1,14 @@
+import {getFirestore, collection, getDocs, query, where} from 'firebase/firestore';
+
 export const get_popular_data = async (setPopularGames) => {
-    const data = await fetch('../../database/DATABASE.json');
-    const data_parsed = await data.json();
     try {
-      const popular_game = data_parsed.filter(game => game.popular === true);
+      const db = getFirestore();
+      const query_collection = collection(db, 'products');
+      const query_collection_filter = query(query_collection, where('popular', "==", true));
+      const docs = await getDocs(query_collection_filter);
+      const popular_game = docs.docs.map(product => ({ id: product.id, ...product.data()}));
       setPopularGames(popular_game);
     } catch (error) {
       console.log(error);
-    }
-}
+    };
+};
